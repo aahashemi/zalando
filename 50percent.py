209407633e10bdac61f837238881c93c391029df
shashemi <aahashemi1380@gmail.com>
2022-06-16 10:48:04 +0200

    new version

--- 50percent.py
+++ 50percent.py
@@ -7,12 +7,15 @@ import pandas as pd
 DATABASE_FILE = 'zalando_database.csv'
 NEW_DATABASE_FILE = 'feature_label.csv'
 df = pd.read_csv(DATABASE_FILE)
-IMAGE_SIZE = 32
+IMAGE_SIZE = 28
 
 
-def saveDatabase(df):
-    df.to_csv(NEW_DATABASE_FILE, index=False)
+def saveDatabase(newdf):
+    newdf.to_csv(NEW_DATABASE_FILE, index=False)
 
+def loadDatabase():
+    newdf = pd.read_csv(NEW_DATABASE_FILE)
+    return newdf
 
 def image_to_array(img_url):
     response = requests.get(img_url)
@@ -22,28 +25,6 @@ def image_to_array(img_url):
     imgArray = np.asarray(image)
     cv_im = cv2.cvtColor(imgArray, cv2.COLOR_BGR2GRAY)
     return cv_im / IMAGE_SIZE
-
-def plot_image(img):
-    #cv_im = image_to_array(img)
-    cv_im = img
-    plt.figure()
-    plt.imshow(cv_im)
-    plt.colorbar()
-    plt.grid(False)
-    plt.show()
-
-def demonstrate():
-    plt.figure(figsize=(10, 10))
-    index = [112, 6268, 7546, 11446, 14612, 21659, 30192, 39202, 46990, 50816]
-    for i in index:
-        plt.subplot(5, 5, index.index(i) + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.imshow(plot_image(df.iloc[i][1]), cmap=plt.cm.binary)
-        plt.xlabel(df.iloc[i][0])
-        plt.colorbar()
-        plt.grid(False)
-    plt.show()
 
 def initiate_dfMain():
     COLUMNS_SIZE = IMAGE_SIZE*IMAGE_SIZE
@@ -59,16 +40,14 @@ def initiate_dfMain():
 
 def main_func():
     print('Initializing dfMain...')
-    dfMain = initiate_dfMain()
-
+    dfMain = loadDatabase()
     print('dfMain Initialized!')
 
     CATEGORIES = {'Jacket':0, 'Pants':1, 'Jeans':2, 'Shorts':3, 'T-shirt':4,
                   'Pullover':5, 'Bag':6, 'Cap':7, 'Sandal':8, 'Skirt':9}
 
-    for row in range(1, len(df)):
+    for row in range(int(len(df)*0.4), int(len(df)*0.5)):
         try:
-
             category_string = df.iloc[row][0]
             category_value = int(CATEGORIES[category_string])
 
@@ -81,7 +60,7 @@ def main_func():
             dfMain = dfMain.append(pd.DataFrame(reshaped_image_array.reshape(1, -1), columns=dfMain.columns), ignore_index=True)
             print(f'Completing => {round((row / len(df)) * 100, 2)}%. Category => {category_string}')
 
-            if row % 50:
+            if row % 100:
                 saveDatabase(dfMain)
                 print('Saved database!')
 
--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-array = np.array([1,2,3,4,5])
-
-r = np.concatenate([[0],array,[77]])
-print(r)
